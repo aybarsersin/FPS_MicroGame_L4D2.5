262691efa446ab51663c929689bef43e799072e0
aybarsersin <aybarsersin@gmail.com>
2020-09-23 22:29:53 +0300

    First Update

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    float maxHp=100, hp;
+
+    void Start()
+    {
+        hp = maxHp;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+        if (hp<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Zombie/ZombieCon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieCon : MonoBehaviour
+{
+    Animator anim;
+    GameObject player;
+    Rigidbody rb;
+    NavMeshAgent nvAgent;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        nvAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 1f)
+        {
+            nvAgent.isStopped = true;
+
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) < 10)
+        {
+            nvAgent.isStopped = false;
+            nvAgent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            nvAgent.isStopped = true;
+        }
+        anim.SetFloat("runSpeed", nvAgent.velocity.magnitude);
+    }
+}
